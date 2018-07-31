ab061388cb1d2366e8a6ebb2d6233e9c6e974242
shvv733kn@gmail.com <shvv733kn@gmail.com>
2018-07-31 13:48:52 +0300

    payments_paypal

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180731095318) do
-=======
-ActiveRecord::Schema.define(version: 20180731095301) do
->>>>>>> 1152f91f843e54f72dca4e570a3787ddfd21990a
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
