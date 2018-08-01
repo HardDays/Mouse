29616406ff4db8a51e3a1fc9a72d2c04fe0abc17
aalbina <aalmukhametova@gmail.com>
2018-08-01 14:08:21 +0300

    fix

--- app/helpers/currency_helper.rb
+++ app/helpers/currency_helper.rb
@@ -4,9 +4,9 @@ class CurrencyHelper
     return [:RUB, :USD, :EUR]
   end
 
-  def ceil2(exp = 0)
+  def ceil2(price, exp = 0)
     multiplier = 10 ** exp
-    ((self * multiplier).ceil).to_f/multiplier.to_f
+    ((price * multiplier).ceil).to_f/multiplier.to_f
   end
 
   def self.convert(price, from_currency, to_currency)
@@ -26,6 +26,6 @@ class CurrencyHelper
     end
 
     new_price = (price.to_i / new_currency_value) * old_currency_value
-    return new_price.ceil2(2)
+    return ceil2(new_price, 2)
   end
 end
