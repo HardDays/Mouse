14b55175943e0d4b3abb61b2abef0a124156b40d
aalbina <aalmukhametova@gmail.com>
2018-08-01 14:22:55 +0300

    fix venue dates prices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801104537) do
+ActiveRecord::Schema.define(version: 20180801112139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -542,8 +542,8 @@ ActiveRecord::Schema.define(version: 20180801104537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "date"
-    t.integer "price_for_daytime"
-    t.integer "price_for_nighttime"
+    t.float "price_for_daytime"
+    t.float "price_for_nighttime"
     t.boolean "is_available", default: true
     t.integer "currency", default: 0
   end
