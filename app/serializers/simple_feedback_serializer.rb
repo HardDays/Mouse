b636c2d185a056be16ed3b51763c5e2f491b2b43
aalbina <aalmukhametova@gmail.com>
2018-08-11 00:13:50 +0300

    address

--- app/serializers/simple_feedback_serializer.rb
+++ app/serializers/simple_feedback_serializer.rb
@@ -1,7 +1,7 @@
 class SimpleFeedbackSerializer < ActiveModel::Serializer
-  attributes :id, :feedback_type, :created_at, :rate_score
+  attributes :id, :feedback_type, :created_at, :rate_score, :sender
 
-  belongs_to :sender
+  # belongs_to :sender
   belongs_to :reply
 
   def feedback_type
@@ -10,5 +10,9 @@ class SimpleFeedbackSerializer < ActiveModel::Serializer
 
   def rate_score
     object.feedback_message.rate_score
+  end
+
+  def sender
+    object.sender.as_json(for_message: true)
   end
 end
