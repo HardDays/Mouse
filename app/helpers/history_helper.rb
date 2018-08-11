7d4d2a06b093797bd9b809e22c7a60ea150e234b
shvv733kn@gmail.com <shvv733kn@gmail.com>
2018-08-11 14:25:35 +0300

    upates_fix

--- app/helpers/history_helper.rb
+++ app/helpers/history_helper.rb
@@ -6,9 +6,9 @@ class HistoryHelper
     EVENT_FIELDS = [:name, :tagline, :description, :date_from, :date_to, :funding_goal,
                     :event_season, :event_year, :event_length, :event_time, :address, :genres,
                     :collaborators, :updates_available, :comments_available, :user_name, :display_name, :phone]
-    VENUE_FIELDS = [:name, :address, :phone, :has_vr, :user_name, :display_name, :phone]
+    VENUE_FIELDS = [:name, :address, :phone, :has_vr, :user_name, :display_name]
     ARTIST_FIELDS = [:name, :about, :user_name, :display_name, :phone, :first_name, :last_name, :stage_name]
     # Enum to write in db
-    ACCOUNT_FIELDS = [:name, :address, :phone, :has_vr, :about, :user_name, :display_name, :phone]
+    ACCOUNT_FIELDS = [:name, :address, :phone, :has_vr, :about, :user_name, :display_name]
 
 end
