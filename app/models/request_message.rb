6a216cca3a9e28b8be579b618aec0e8967ac1361
shvv733kn@gmail.com <shvv733kn@gmail.com>
2018-08-14 15:04:32 +0300

    price

--- app/models/request_message.rb
+++ app/models/request_message.rb
@@ -22,8 +22,8 @@ class RequestMessage < ApplicationRecord
     res.delete('updated_at')
 
     if options[:user]
-      res[:original_price] = estimated_price
-      res[:price] = CurrencyHelper.convert(estimated_price, currency, options[:user].preferred_currency) if estimated_price != nil
+      res[:original_price] = price
+      res[:price] = CurrencyHelper.convert(price, currency, options[:user].preferred_currency) if price != nil
     end
 
     res[:event_info] = event.as_json(user: options[:user])
