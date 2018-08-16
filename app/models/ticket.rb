e7be0e917fd4b39a2bb172d4fd9fef8801cb9a9f
shvv733kn@gmail.com <shvv733kn@gmail.com>
2018-08-16 11:07:43 +0300

    min_age

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -10,6 +10,12 @@ class Ticket < ApplicationRecord
     res = super
     res[:type] = tickets_type ? tickets_type.name : nil
 
+    if event.venue and event.venue.public_venue
+      res[:min_age] = event.venue.public_venue.min_age
+    else
+      res[:min_age] = nil
+    end
+
     if options[:user]
       res[:original_price] = price
       res[:price] = CurrencyHelper.convert(price, currency, options[:user].preferred_currency)
