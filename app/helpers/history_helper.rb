ab274e69b602e801c762a99d10aacfd802b368ca
aalbina <aalmukhametova@gmail.com>
2018-09-04 15:52:58 +0300

    fix

--- app/helpers/history_helper.rb
+++ app/helpers/history_helper.rb
@@ -6,6 +6,7 @@ class HistoryHelper
     EVENT_FIELDS = [:description]
     VENUE_FIELDS = [:address, :phone, :has_vr, :user_name, :display_name]
     ARTIST_FIELDS = [:stage_name, :about, :user_name, :display_name, :phone]
+    ACCOUNT_FIELDS = [:address, :phone, :has_vr, :about, :user_name, :display_name, :image, :gallery_image, :stage_name]
 
     # Enum to write in db
     FIELDS = [:description, :address, :phone, :has_vr, :about, :user_name, :display_name, :image, :gallery_image, :stage_name]
