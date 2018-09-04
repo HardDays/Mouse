261fec52cc1c52bd2b167f78e4319ad176d63188
aalbina <aalmukhametova@gmail.com>
2018-09-04 15:18:26 +0300

    wip

--- app/models/feed_item.rb
+++ app/models/feed_item.rb
@@ -1,36 +1,35 @@
 class FeedItem < ApplicationRecord
-  enum action: HistoryHelper::ACTIONS, _suffix: true
-  enum field: HistoryHelper::FIELDS, _suffix: true
-
-  belongs_to :account, optional: true
-  belongs_to :event, optional: true
+  belongs_to :event_update, optional: true
+  belongs_to :account_update, optional: true
 
   has_many :comments
   has_many :likes
 
   def as_json(options={})
     res = super
+    res.delete("event_update_id")
+    res.delete("account_update_id")
 
     res[:comments] = comments.count
     res[:likes] = likes.count
     res[:is_liked] = likes.where(user_id: options[:user].id).exists?
 
-    if event_id
+    if event_update
       res[:type] = "event_update"
-      res[:action] = [action, field].compact.join("_")
-      res[:event] = event.as_json(only: [:id, :name, :comments_available])
-      res[:account] = event.creator.as_json(:only => [:id, :user_name, :image_id])
+      res[:action] = [event_update.action, event_update.field].compact.join("_")
+      res[:event] = event_update.event.as_json(only: [:id, :name, :comments_available])
+      res[:account] = event_update.event.creator.as_json(:only => [:id, :user_name, :image_id])
 
-      if action == 'add_ticket'
-        res[:ticket] = event.tickets.where(id: value.to_i)
+      if event_update.action == 'add_ticket'
+        res[:ticket] = event_update.event.tickets.where(id: event_update.value.to_i)
       else
-        res[:value] = value
+        res[:value] = event_update.value
       end
-    elsif account_id
+    elsif account_update
       res[:type] = "account_update"
-      res[:action] = [action, field].compact.join("_")
-      res[:account] = account
-      res[:value] = value
+      res[:action] = [account_update.action, account_update.field].compact.join("_")
+      res[:account] = account_update.account
+      res[:value] = account_update.value
     end
 
     return res
