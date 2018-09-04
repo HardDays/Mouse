37f90b6276f0b8963ad3a811dcfc7d5e338ed2fd
aalbina <aalmukhametova@gmail.com>
2018-09-04 15:46:32 +0300

    deletions

--- app/models/account_update.rb
+++
@@ -1,6 +0,0 @@
-class AccountUpdate < ApplicationRecord
-    enum action: HistoryHelper::ACCOUNT_ACTIONS, _suffix: true
-    enum field: HistoryHelper::ACCOUNT_FIELDS, _suffix: true
-
-    belongs_to :account
-end
