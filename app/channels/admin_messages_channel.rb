f5d760ea91066e148e16ae8293876d1ff177926f
aalbina <aalmukhametova@gmail.com>
2018-09-10 21:05:22 +0300

    revert

--- app/channels/admin_messages_channel.rb
+++ app/channels/admin_messages_channel.rb
@@ -2,13 +2,10 @@ class AdminMessagesChannel < ApplicationCable::Channel
 
   def subscribed
     stream_for user_id
-  end
 
-  def received(data)
-    if data == "count"
-      count = AdminMessage.where(is_read: false).where.not(sender_id: user_id).count
-      AdminMessagesChannel.broadcast_to(user_id, count: count)
-    end
+    count = AdminMessage.where(is_read: false).where.not(sender_id: user_id).count
+    AdminMessagesChannel.broadcast_to(user_id, count: count)
   end
+  
 end
 
