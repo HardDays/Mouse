a79c3a06ac53bdb9287f44a75c103da0c5ffaad8
aalbina <aalmukhametova@gmail.com>
2018-09-10 20:34:54 +0300

    fix send messages

--- app/controllers/admin_messages_controller.rb
+++ app/controllers/admin_messages_controller.rb
@@ -104,13 +104,13 @@ class AdminMessagesController < ApplicationController
         if message.save
           count = AdminMessage.where(is_read: false).where.not(sender_id: receiver.id).count
           ApplicationCable::AdminMessagesChannel.broadcast_to(receiver.id, count: count)
-
-          render status: :created
         else
           topic.destroy
         end
       end
     end
+
+    render status: :created
   end
 
   # POST /admin/messages
