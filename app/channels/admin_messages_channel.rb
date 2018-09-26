ee251009bae6ee2b81f349d87fbc486d1d6b16c0
aalbina <aalmukhametova@gmail.com>
2018-09-27 00:14:42 +0300

    fix

--- app/channels/admin_messages_channel.rb
+++ app/channels/admin_messages_channel.rb
@@ -1,7 +1,7 @@
 class AdminMessagesChannel < ApplicationCable::Channel
 
   def subscribed
-    stream_for admin_id
+    stream_for user_id
 
     count = AdminTopic.where(sender_id: user_id).messages.where(is_read: false).where.not(sender_id: user_id).count +
       AdminTopic.where(receiver_id: user_id).messages.where(is_read: false).where.not(sender_id: user_id).count
