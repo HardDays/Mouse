94e16157e5b5334db1e7bd6a3c70d6ed7c74a729
aalbina <aalmukhametova@gmail.com>
2018-10-02 19:57:29 +0300

    capacity

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -42,6 +42,7 @@ class Venue < ApplicationRecord
             attrs[:display_name] = account.display_name
             attrs[:user_name] = account.user_name
             attrs[:image_id] = account.image_id
+            attrs[:capacity] = capacity
 
             #attrs[:price] = nil
             return attrs
