ea2576c577bbf5d9d6a0d6f9f31885ee3044f2c8
shvv733kn@gmail.com <shvv733kn@gmail.com>
2018-12-05 16:06:25 +0300

    questions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
       post :reply
     end
   end
+  get 'questions/my', action: :my, controller: 'questions'
+
+
   resources :feedbacks, only: [:index, :create, :show]
 
   #Auth routes
